130aa1ff6bd5a221e017fcd93bc5ef0011b33061
Pavel Krasnik <pkrasnik@ultimeta.ru>
2022-10-05 19:29:08 +0300

    remove deprecated configuration

--- src/main/java/pk/test/exchange/repository/CurrencyRepository.java
+++ src/main/java/pk/test/exchange/repository/CurrencyRepository.java
@@ -3,7 +3,5 @@ package pk.test.exchange.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pk.test.exchange.model.Currency;
 
-import java.util.Optional;
-
 public interface CurrencyRepository extends JpaRepository<Currency, String> {
 }
--- src/main/java/pk/test/exchange/security/SecurityConfiguration.java
+++ src/main/java/pk/test/exchange/security/SecurityConfiguration.java
@@ -3,26 +3,12 @@ package pk.test.exchange.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfiguration(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+public class SecurityConfiguration {
 
     @Bean
     @Profile("dev")
